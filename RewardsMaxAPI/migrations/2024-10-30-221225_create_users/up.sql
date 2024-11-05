5cf0b536e7955b88c8eeafc31a16a41bf40b7a0a
Daniel Del Río <danny@ens-labs.com>
2024-11-05 15:51:27 -0500

    Preparing for merge

--- RewardsMaxAPI/migrations/2024-10-30-221225_create_users/up.sql
+++
@@ -1,9 +0,0 @@
--- Your SQL goes here
-CREATE TABLE users (
-    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    created DATETIME NOT NULL,
-    email TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL,
-    updated DATETIME NOT NULL,
-    username TEXT NOT NULL UNIQUE
-);
