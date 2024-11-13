3634f65694365674fbfaad7ec9e99cbe987e4b07
Victoria Crouthamel <victoriacrouthamel@gmail.com>
2024-11-12 23:40:41 -0700

    Began adding in star rating system

--- Frontend/app/controllers/card-details.js
+++ Frontend/app/controllers/card-details.js
@@ -1,39 +1,51 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
-import { service } from '@ember/service';
 
 export default class CardDetailsController extends Controller {
-  @service router;
 
-  // Navigate to home
+  // Set the active class for clicked star
+  @action
+  setActiveStar(event) {
+    const selectedStar = event.target.dataset.star;
+
+    // Loop over all the stars and remove "active" class
+    for (let i = 1; i <= 5; i++) {
+      const star = document.getElementById(`star${i}`);
+      star.classList.remove('active');
+    }
+
+    // Add the "active" class to the selected star and all stars before it
+    for (let i = 1; i <= selectedStar; i++) {
+      const star = document.getElementById(`star${i}`);
+      star.classList.add('active');
+    }
+  }
+
+  // Navigate to other pages
   @action
   home(event) {
     event.preventDefault();
     this.router.transitionTo('home');
   }
 
-  // Navigate to wallet
   @action
   wallet(event) {
     event.preventDefault();
     this.router.transitionTo('wallet');
   }
 
-  // Navigate to profile
   @action
   profile(event) {
     event.preventDefault();
     this.router.transitionTo('profile');
   }
 
-  // Navigate to crowdsourcing
   @action
   crowdsourcing(event) {
     event.preventDefault();
     this.router.transitionTo('crowdsourcing');
   }
 
-  // Future functionality for product search
   @action
   results(event) {
     event.preventDefault();
--- Frontend/app/routes/card-details.js
+++ Frontend/app/routes/card-details.js
@@ -1,8 +1,10 @@
+// app/routes/card-details.js
 import Route from '@ember/routing/route';
 
 export default class CardDetailsRoute extends Route {
   model(params) {
     return {
+      cardId: params.cardId,  // Dynamic segment for cardId
       cardName: params.cardName,
       cardBenefits: params.cardBenefits,
     };
