2065ae6d82040ed1aff5fe1f7b8723df96825dbb
Correy Xu <cxu002@odu.edu>
2024-11-22 17:45:55 -0500

    searchcompany endpoint

--- Backend/RewardsMaxAPI/src/web/companies.rs
+++ Backend/RewardsMaxAPI/src/web/companies.rs
@@ -7,15 +7,38 @@ use crate::web::lib::establish_connection;
 pub fn router() -> Router<()> {
     Router::new()
         .route("/addCompany", post(self::post::add_company))
-        .route("/addCompany", get(self::get::add_company))
+        .route("/searchCompany", get(self::get::search_company))
         .route("/viewCompanies", get(self::get::view_companies))
 }
 
 mod get {
     use super::*;
     
-    pub async fn add_company() -> &'static str {
-        "Add company endpoint"
+    pub async fn search_company(query: Option<String>) -> impl IntoResponse {
+        use crate::web::schema::companies::dsl::*;
+        let mut connection = establish_connection();
+
+
+        
+        let query1 = "SELECT * FROM companies";
+        let query2 = match query {
+            Some(search) => format!("{} WHERE name LIKE '%{}%'", query1, search),
+            None => query1.to_string(),
+        };
+
+    
+        let results = sql_query(query2).load::<Company>(&mut connection);
+
+
+
+        match results{
+            Ok(companies_list) => (StatusCode::OK, Json(companies_list)).into_response(),
+            Err(e) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to retrieve data: {}", e),
+            )
+            .into_response(),
+        }
     }
 
     // Able to read from DB with straight SQL, plz leave it alone
