43cdd035e0568ee707c0789508655369410d6cf5
bhear005 <bhear005@odu.edu>
2024-12-05 17:45:03 -0500

    Re-add routing to results page

--- Frontend/app/controllers/search.js
+++ Frontend/app/controllers/search.js
@@ -1,5 +1,14 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { service } from '@ember/service';
 
 export default class Search extends Controller {
+  @service router;
 
+  // Future functionality for product search
+  @action
+  results(event) {
+    event.preventDefault();
+    this.router.transitionTo('search_results');
+  }
 }
