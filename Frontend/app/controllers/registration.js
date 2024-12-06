63d866ee4b20ddb0a699884e9bd79c5c20b70d3f
bhear005 <bhear005@odu.edu>
2024-12-05 20:43:11 -0500

    Add password rules for account creation and placeholder signup API call

--- Frontend/app/controllers/registration.js
+++ Frontend/app/controllers/registration.js
@@ -4,8 +4,12 @@ import { action } from '@ember/object';
 import { service } from '@ember/service';
 
 export default class Registration extends Controller {
+  @tracked email = '';
   @tracked username = '';
   @tracked password = '';
+  @tracked passwordTwo = '';
+  @tracked errorMessage = '';
+  passwordError = false;
   @service router;
 
   @action
@@ -21,18 +25,79 @@ export default class Registration extends Controller {
   @action
   setPassword(event) {
     this.password = event.target.value;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    if (this.password === '') {
+      this.passwordError = false;
+      this.errorMessage = '';
+    } 
+    else {
+      const isValidPassword = passwordRegex.test(this.password);
+      this.passwordError = !isValidPassword;
+      this.errorMessage = isValidPassword ? '' : 'Password must contain at least 8 characters, one uppercase letter, one lowercase letter, one number, and one special character.';
+    }
+  }
+
+  @action
+  setPasswordTwo(event) {
+    this.passwordTwo = event.target.value;
+    
+    if (this.passwordTwo === '') {
+      this.errorMessage = '';
+    } 
+    else if (this.password !== this.passwordTwo) {
+      this.errorMessage = 'Passwords do not match!';
+      this.passwordError = true;
+    } 
+    else {
+      this.errorMessage = '';
+      this.passwordError = false;
+    }
   }
 
   @action
   async handleRegistration(event) {
     event.preventDefault();
 
-    // const formBody = JSON.stringify({
-    //     email: this.email,
-    //     username: this.username,
-    //     password: this.password,
-    // });
+    // If there are no errors with the password, send a POST request to the server
+    // Server side signup TBD
+    // if (this.passwordError === false) {
 
-    this.transitionTo('login');
+    //   try{
+    //     let response = await fetch('http://localhost:8080/signup', {
+    //       method: 'POST',
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //       },
+    //       body: JSON.stringify({
+    //         email: this.email,
+    //         username: this.username,
+    //         password: this.password,
+    //       }),
+    //     });
+
+    //     if (!response.ok) {
+    //       throw new Error('Registration failed');
+    //     }
+    //     this.router.transitionTo('login');
+    //   } 
+    //   catch (error) {
+    //     this.errorMessage = error.message;
+    //   }
+    // }
+  }
+
+  @action
+  togglePasswordVisibility(event) {
+    const passwordField = event.target.closest('.input-group').querySelector('input');
+    passwordField.type = passwordField.type === 'password' ? 'text' : 'password';
+    event.target.classList.toggle('fa-eye');
+    event.target.classList.toggle('fa-eye-slash');
+  }
+
+  // Return to login page
+  @action
+  back() {
+    this.router.transitionTo('login');
   }
 }
