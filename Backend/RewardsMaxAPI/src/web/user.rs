89dcac2d0188a868eb085e7c465516eac55a58fe
bhear005 <bhear005@odu.edu>
2024-12-08 23:04:36 -0500

    Add attempt at get user_id

--- Backend/RewardsMaxAPI/src/web/user.rs
+++ Backend/RewardsMaxAPI/src/web/user.rs
@@ -1,12 +1,13 @@
-use axum::{routing::post, Router, Json, response::IntoResponse};
+use axum::{routing::{post, get}, Router, Json, response::IntoResponse, extract::Path};
 use diesel::{prelude::*};
-use crate::web::models::{NewUser};
+use crate::web::models::NewUser;
 use crate::web::lib::establish_connection;
 use password_auth::generate_hash;
 
 pub fn router() -> Router<()> {
     Router::new()
         .route("/signup", post(self::post::signup_user))
+        // .route("/users/:username", get(self::get::get_user_id))
 }
 
 mod post {
@@ -32,3 +33,21 @@ mod post {
         }
     }
 }
+
+// mod get {
+//     use super::*;
+//     use crate::web::schema::users::dsl::*;
+
+//     pub async fn get_user_id(Path(username): Path<String>) -> impl IntoResponse {
+//         let connection = establish_connection();
+
+//         match users.filter(username.eq(&username)).first::<i32>() {
+//             Ok(user) => (axum::http::StatusCode::OK, user).into_response(),
+//             Err(e) => (
+//                 axum::http::StatusCode::INTERNAL_SERVER_ERROR,
+//                 format!("Failed to get user: {}", e),
+//             )
+//             .into_response(),
+//         }
+//     }
+// }
